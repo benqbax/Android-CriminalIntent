035cb3582394dc4865f1999d374862c2c9f66852
Ben Forsrup <ben_forsrup@hotmail.com>
2017-05-24 12:01:32 +0200

    Page 200

---
+++ app/src/main/java/com/example/benforsrup/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.example.benforsrup.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Ben on 2017-05-24.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity{
+    @Override
+    protected Fragment createFragment(){
+        return new Fragment();
+    }
+}
--- app/src/main/java/com/example/benforsrup/criminalintent/CriminalActivity.java
+++ app/src/main/java/com/example/benforsrup/criminalintent/CriminalActivity.java
@@ -7,20 +7,10 @@ import android.os.Bundle;
 
 
 
-public class CriminalActivity extends AppCompatActivity {
+public class CriminalActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-            String testVar = "testVarAgain";
-        }
+    protected Fragment createFragment(){
+        return new CrimeFragment();
     }
 }
